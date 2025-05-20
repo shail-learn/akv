1d74873cbd64b7999d94ed8f734174b013ba2ad8
shailup50 <shailendra@cruxcreativesolutions.com>
2025-05-20 12:00:44 +0530

    start sofwood

--- src/app/agroForestry/timber/softwood-light-timber-trees/page.js
+++ src/app/agroForestry/timber/softwood-light-timber-trees/page.js
@@ -1,8 +1,21 @@
+
+import { Banner, Plantation, Softwood, Species } from '@/app/components/softwood/BannerSoftwoodSpeciesPlantation'
+import { Choose, Commercial, Faqs, Why } from '@/app/components/softwood/CommercialWhyChooseFaqs'
 import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <>
+      <Banner />
+      <Softwood />
+      <Species />
+      <Plantation />
+      <Commercial />
+      <Why />
+      <Choose />
+      <Faqs />
+
+    </>
   )
 }
 
