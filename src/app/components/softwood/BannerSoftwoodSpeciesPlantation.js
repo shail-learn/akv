67cdff9a086243524afa18e1d535367a0dd5d389
shailup50 <shailendra@cruxcreativesolutions.com>
2025-05-20 17:19:30 +0530

    start sofwood page design

--- src/app/components/softwood/BannerSoftwoodSpeciesPlantation.js
+++ src/app/components/softwood/BannerSoftwoodSpeciesPlantation.js
@@ -5,6 +5,19 @@ import BannerPage from '../BannerPage';
 import banner from "../../../assets/images/softwood/banner.webp";
 import { Wave } from '../Wave';
 import basket from "../../../assets/images/softwood/close-up-wood.webp";
+import why1 from "../../../assets/images/softwood/why1.webp";
+import why2 from "../../../assets/images/softwood/why2.webp";
+import why3 from "../../../assets/images/softwood/why3.webp";
+import why4 from "../../../assets/images/softwood/why4.webp";
+import why5 from "../../../assets/images/softwood/why5.webp";
+import img1 from "../../../assets/images/softwood/image1.webp";
+import img2 from "../../../assets/images/softwood/image2.webp";
+import img3 from "../../../assets/images/softwood/image3.webp";
+
+
+
+import { Gridbox } from '../Gridbox';
+import { Iconslider } from '../Iconslider';
 
 export const Banner = () => {
   const text = {
@@ -30,49 +43,121 @@ export const Softwood = () => {
 
     note: `Their lightweight yet durable nature makes them ideal for applications requiring flexibility and workability. <br/>
     At Atulye Krishi Vana, we specialize in agroforestry solutions that promote sustainable timber cultivation. Our plantations include premium softwood species like Haldu (Haldina cordifolia), Pakad (Ficus virens), and Pipal (Ficus religiosa)—each offering unique properties for commercial and environmental benefits. Whether you are a farmer, investor, or business, we help you establish high-yield plantations that maximize returns while preserving nature. `,
-  image: basket,
+    image: basket,
   };
-return (
-  <>
-    <Wave />
-    <section className="py-12 px-4 md:px-8 lg:px-16 bg-[#1B453C]">
-      <div className='mx-auto max-w-7xl px-4 md:px-6 lg:px-8'>
-        <div className="flex flex-col-reverse justify-between lg:flex-row items-center gap-8">
-          <div className="w-full md:w-1/2">
+  return (
+    <>
+      <Wave />
+      <section className="py-12 px-4 md:px-8 lg:px-16 bg-[#1B453C]">
+        <div className='mx-auto max-w-7xl px-4 md:px-6 lg:px-8'>
+          <div className="flex flex-col-reverse justify-between lg:flex-row items-center gap-8">
+            <div className="w-full md:w-1/2">
 
-            <p className="mt-0 md:mt-4 text-white" dangerouslySetInnerHTML={{__html: basketData.note}}></p>
-          </div>
+              <p className="mt-0 md:mt-4 text-white" dangerouslySetInnerHTML={{ __html: basketData.note }}></p>
+            </div>
 
-          <div className="w-full md:w-[40%]">
-            <div className="rounded-xl overflow-hidden">
-              <Image
-                src={basketData.image}
-                alt="Basket of fruits and vegetables"
-                width={600}
-                height={400}
-                className="object-cover w-full h-auto"
-              />
+            <div className="w-full md:w-[40%]">
+              <div className="rounded-xl overflow-hidden">
+                <Image
+                  src={basketData.image}
+                  alt="Basket of fruits and vegetables"
+                  width={600}
+                  height={400}
+                  className="object-cover w-full h-auto"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </>
-)
+      </section>
+    </>
+  )
 }
 
 
 
 export const Species = () => {
+  const whyChooseData = [
+    {
+      image: why1,
+      title: 'Workability & Flexibility',
+      description: 'Light timber is easier to saw, carve, and polish, making it highly desirable for furniture, musical instruments, and interior decorations.',
+    },
+    {
+      image: why2,
+      title: 'Multipurpose Utility',
+      description: 'These species provide fodder, medicinal benefits, and ecosystem services. Pakad wood is widely used in plywood and packing industries.',
+    },
+    {
+      image: why3,
+      title: 'Fast Growth & High Yield',
+      description: 'Softwood species mature faster than hardwoods, allowing for quicker harvesting cycles. Haldu and Pakad trees can be ready for timber within 10–15 years.',
+    },
+    {
+      image: why4,
+      title: 'Sustainable & Eco-Friendly',
+      description: 'These trees contribute to carbon sequestration, improve air quality, and support biodiversity by preventing soil erosion.',
+    },
+    {
+      image: why5,
+      title: 'Low Maintenance & High Adaptability',
+      description: 'These species thrive in diverse climates and soil types, requiring minimal care once established.',
+    },
+  ];
+
   return (
-    <div>Species</div>
+    <>
+      <Gridbox heading="Why Choose Softwood & Light Timber Trees?" data={whyChooseData} />
+    </>
   )
 }
 
 
 
 export const Plantation = () => {
+  const heading = "Species Overview";
+  const custom_class = "original_img padding_argust"
+  const cardData = [
+    {
+      title: "Pipal (Ficus religiosa)",
+      icon: img1,
+      description: "Uses: Religious structures, decorative carving, and medicinal applications",
+    },
+    {
+      title: "Haldu (Haldina cordifolia)",
+      icon: img2,
+
+      description: "Uses:Furniture, plywood, sports goods, handicrafts, and interior paneling",
+    },
+    {
+      title: "Pakad (Ficus virens)",
+      icon: img3,
+
+      description: "Uses: Plywood, crates, agricultural implements, and tool handles",
+    },
+    {
+      title: "Pipal (Ficus religiosa)",
+      icon: img1,
+      description: "Uses: Religious structures, decorative carving, and medicinal applications",
+    },
+    {
+      title: "Haldu (Haldina cordifolia)",
+      icon: img2,
+
+      description: "Uses:Furniture, plywood, sports goods, handicrafts, and interior paneling",
+    },
+
+  ];
+
+
+
   return (
-    <div>Plantation</div>
-  )
+    <>
+      <Iconslider
+        card={cardData}
+        heading={heading}
+        custom_class={custom_class}
+      />
+    </>
+  );
 }
