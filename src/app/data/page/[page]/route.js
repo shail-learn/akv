ebea4752ddb60ffe7bedb743e508b57748f8e75b
zeesee123 <zubinchadha@gmail.com>
2025-05-21 18:57:37 +0530

    adding in the changes

---
+++ src/app/data/page/[page]/route.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export async function GET(req, {params}) {
+  
+  const page = params.page; // ← This will be 'homepage' in /api/page/homepage
+
+  const response= await axios.get(`https://www.akv.org.in/api/${page}`);
+  
+  const data=response.data;
+
+  // if (page === 'homepage') {
+    
+  //  const data=axios.get(`https://www.akv.org.in/api`);
+    
+  //   return res.status(200).json({ status:'success',data:data });
+
+  // }
+
+  const page_exists=false;
+
+  switch(page){
+    case 'homepage':
+        page_exists=true;
+    break;
+    default:
+        console.log('page does not exist');
+        break;
+
+  }
+
+  if (page_exists) {
+    
+   
+    
+      return Response.json({
+      status: 'success',
+      data: data,
+    });
+
+  }
+
+
+
+  return Response.json(
+      { status:'error', message: 'Failed to fetch data' },
+      { status: 500 }
+    );
+
+}
