cb486ad92f9ad4a68c1d6528cab64f874e5bea3a
shailup50 <shailendra@cruxcreativesolutions.com>
2025-07-03 00:17:04 +0530

    project page design shri-mukund-mausam-vedhshala &  sacred-and-culturally-trees , agricultural-and-kitchen-crops img

---
+++ src/app/our-projects/shri-mukund-mausam-vedhshala/page.js
@@ -0,0 +1,19 @@
+import { Mukund, Key, Des, History, Uses, Mausam } from '@/app/components/mukund/MukundKeyDesHistoryUsesMausam'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <Mukund/>
+    <Key/>
+    <Des/>
+    <History/>
+    <Uses/>
+    <Mausam/>
+
+
+    </>
+  )
+}
+
+export default page
