bfc62f30c3a14c52833f67a6bdcd01fcdfea5d00
shailup50 <shailendra@cruxcreativesolutions.com>
2025-07-10 18:42:14 +0530

    2 page projet page design 3 page redesign

--- src/app/components/shriBhrigu/BhriguConceptTableSpiritualHistoricalVan.js
+++ src/app/components/shriBhrigu/BhriguConceptTableSpiritualHistoricalVan.js
@@ -13,6 +13,8 @@ import bgstay from "../../../assets/images/stress/bg.webp";
 import { MarketLayout } from '../utilities/UsesStanadarLayout';
 import img01 from "../../../assets/images/teak/small.webp";
 import PartnerBg from "../../../assets/images/respirtaory/bgimg.webp";
+import image2 from "../../../assets/images/projects/project20/key.webp";
+import { RiCheckboxCircleFill } from "react-icons/ri";
 
 
 export const Bhrigu = () => {
@@ -26,24 +28,42 @@ export const Bhrigu = () => {
     return (
         <>
             <BannerPage heading={text.heading} subtitle={text.subtitle} title={text.title} banner={text.banner} opacity={text.opacity} />
-            <Wave />
+            {/* <Wave /> */}
         </>
     )
 }
 
 
 export const Concept = () => {
-    const basketData = {
-        note: `
-        Theme Index: Navagrah <br/>
-        Location Name: Shri. Bhrigu Van <br/>
-        Coordinates: 22°59'40.3"N 77°10'47.2"E <br/>
-        Area: 3135 square meters <br/>
-        Date of Planting: 14 November 2021 <br/>
-        Distance from Admin Block: 670 meters
-        `,
-        image: info,
-    };
+    const heading = "Key Details"
+    const infoData = [
+        {
+            title: "Theme Index",
+            description: "Navagrah",
+        },
+        {
+            title: "Location Name",
+            description: "Shri. Bhrigu Van",
+        },
+        {
+            title: "Coordinates",
+            description: `22°59'40.3"N 77°10'47.2"E`,
+        },
+        {
+            title: "Area",
+            description: "3135 square meters",
+        },
+        {
+            title: "Date of Planting",
+            description: "14 November 2021",
+        },
+
+        {
+            title: "Distance from Admin Block",
+            description: "670 meters",
+        },
+    ];
+
 
     const shortinfo = "In Hindu philosophy and astrology, Navagrahas—the nine celestial influencers—have deep effects on human life. Shri. Bhrigu Van is inspired by this tradition, using plants that are believed to resonate with each Graha (planet), Nakshatra (star), and Rashi (zodiac sign)."
 
@@ -71,23 +91,30 @@ export const Concept = () => {
     return (
         <>
 
-            <section className="py-12 md:!pt-0  px-4 md:px-8 lg:px-16 bg-[#1B453C]">
-                <div className='mx-auto max-w-7xl px-4 md:px-6 lg:px-8'>
-                    <div className="flex flex-col-reverse justify-between lg:flex-row   gap-8">
-                        <div className="w-full md:w-1/2">
-                            <h2 className='text-white text-2xl md:text-3xl font-medium mb-6'>Key Details</h2>
-                            <p className="mt-0 md:mt-4 text-white redhat" dangerouslySetInnerHTML={{ __html: basketData.note }} ></p>
-                        </div>
-                        <div className="w-full md:w-[40%]">
-                            <div className="rounded-xl overflow-hidden">
-                                <Image
-                                    src={basketData.image}
-                                    alt="Basket of fruits and vegetables"
-                                    width={600}
-                                    height={400}
-                                    className="object-cover w-full h-auto"
-                                />
+            <section className='relative text-center md:text-left   pb-16  pt-20 lg:pb-16 bg-[#f5f5f5]'  >
+                <div className='mx-auto max-w-7xl px-4 md:px-6 mx:px-2 relative z-1 '>
+                    <div className='flex flex-col lg:flex-row justify-between items-center gap-12'>
+                        <div className='md:w-[40%] w-full  w-12/12'>
+                            <div className='relative'>
+                                <Image className='w-full h-full rounded-xl' src={image2} alt='banner' />
+
                             </div>
+                        </div>
+                        <div className='md:w-[60%]  w-full  w-12/12'>
+                            <h2 className='text-3xl md:leading-[1.3] md:text-4xl font-medium redhat text-[#1B453C] mb-8'>{heading}</h2>
+
+                            <div className='my-6 mb-14'>
+                                {infoData.map((item, index) => (
+                                    <div key={index} className="flex items-start space-x-3 mb-4 py-1">
+                                        <RiCheckboxCircleFill className="h-5 w-5 text-green-800 mt-1" />
+                                        <div>
+                                            <h3 className="font-semibold redhat text-left text-lg">{item.title}</h3>
+                                            <p className="text-black redhat">{item.description}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -285,53 +312,53 @@ export const Spiritual = () => {
 
     const boxslider = [
         {
-          image: impact1,
-          heading: "Spiritual Uses",
-          description: [
-            "Astrological Remedies: Planting specific trees helps balance planetary influences",
-            "Meditation & Worship: Enhances connection with cosmic energies",
-            "Vastu Shastra: Aligns environmental energies for health and prosperity",
-          ]
+            image: impact1,
+            heading: "Spiritual Uses",
+            description: [
+                "Astrological Remedies: Planting specific trees helps balance planetary influences",
+                "Meditation & Worship: Enhances connection with cosmic energies",
+                "Vastu Shastra: Aligns environmental energies for health and prosperity",
+            ]
         },
         {
-          image: impact1,
-          heading: "Medicinal Uses",
-          description: [
-            "Ayurvedic Healing: Each plant has known therapeutic properties",
-            "Mental and Emotional Balance: Trees promote calmness and resilience",
-            "Holistic Well-being: Supports physical, spiritual, and ecological health",
+            image: impact1,
+            heading: "Medicinal Uses",
+            description: [
+                "Ayurvedic Healing: Each plant has known therapeutic properties",
+                "Mental and Emotional Balance: Trees promote calmness and resilience",
+                "Holistic Well-being: Supports physical, spiritual, and ecological health",
 
-          ]
+            ]
         },
 
-      ]
+    ]
     return (
-      <>
+        <>
 
-        <section
-          className="relative mt-10 pt-16 pb-16 lg:pt-20 lg:pb-20" style={{ background: `url(${bgstay.src})center center / cover no-repeat` }}>
-          <div className='absolute top-0 left-0 w-full h-full bg-black opacity-50'></div>
-          <div className='mx-auto max-w-[1400px] md:mr-0 px-4 md:px-8 lg:px-2 '>
-            <div className='flex flex-wrap lg:flex-nowrap justify-between  gap-6 px-0 md:px-6 z-10 relative lg:px-2'>
-              <div className='w-full lg:w-3/12 md:mt-10'>
-                <div className='flex justify-between   flex-wrap'>
-                  <h2 className="lg:leading-snug  text-white redhat mb-4 lg:mb-7 text-center lg:text-left text-3xl md:text-4xl  font-medium  w-full">
-                    {heading}
-                  </h2>
+            <section
+                className="relative mt-10 pt-16 pb-16 lg:pt-20 lg:pb-20" style={{ background: `url(${bgstay.src})center center / cover no-repeat` }}>
+                <div className='absolute top-0 left-0 w-full h-full bg-black opacity-50'></div>
+                <div className='mx-auto max-w-[1400px] md:mr-0 px-4 md:px-8 lg:px-2 '>
+                    <div className='flex flex-wrap lg:flex-nowrap justify-between  gap-6 px-0 md:px-6 z-10 relative lg:px-2'>
+                        <div className='w-full lg:w-3/12 md:mt-10'>
+                            <div className='flex justify-between   flex-wrap'>
+                                <h2 className="lg:leading-snug  text-white redhat mb-4 lg:mb-7 text-center lg:text-left text-3xl md:text-4xl  font-medium  w-full">
+                                    {heading}
+                                </h2>
+                            </div>
+                        </div>
+
+                        <div className='w-full lg:w-8/12'>
+                            <SliderBlur boxslider={boxslider} class2={class2} />
+
+                        </div>
+                    </div>
+
                 </div>
-              </div>
 
-              <div className='w-full lg:w-8/12'>
-                <SliderBlur boxslider={boxslider} class2={class2} />
+            </section>
 
-              </div>
-            </div>
-
-          </div>
-
-        </section>
-
-      </>
+        </>
     )
 }
 
@@ -343,43 +370,43 @@ export const Historical = () => {
         description: "The concept of Navagrah originates in Vedic astrology and is rooted in texts such as the Brihat Parashara Hora Shastra. The garden is a reflection of these age-old beliefs, bringing together:",
 
         steps: [
-          "Spiritual traditions",
-          "Cultural heritage",
-          "Ecological sustainability",
+            "Spiritual traditions",
+            "Cultural heritage",
+            "Ecological sustainability",
         ],
 
-      };
-      return (
+    };
+    return (
         <>
-          <MarketLayout marketData={marketData} />
+            <MarketLayout marketData={marketData} />
 
         </>
-      );
+    );
 }
 
 
 export const Van = () => {
     const heading = "Shri. Bhrigu Van – Navagrah Garden";
     const description =
-      "Shri. Bhrigu Van – Navagrah Garden is not just a plantation—it is a cosmic experience on earth. Each plant, each direction, and each step in this garden is aligned with celestial wisdom. It invites visitors to explore the intersection of astrology, ecology, and wellness, while contributing to biodiversity and environmental education.<br/> <br/> This garden exemplifies how ancient knowledge and natural systems can be harmonized to create a space of lasting significance.";
+        "Shri. Bhrigu Van – Navagrah Garden is not just a plantation—it is a cosmic experience on earth. Each plant, each direction, and each step in this garden is aligned with celestial wisdom. It invites visitors to explore the intersection of astrology, ecology, and wellness, while contributing to biodiversity and environmental education.<br/> <br/> This garden exemplifies how ancient knowledge and natural systems can be harmonized to create a space of lasting significance.";
 
     return (
-      <section
-        className="relative bg-no-repeat py-14 !mt-0 lg:py-32 bg-cover w-full my-8 md:my-12  "
-        style={{ backgroundImage: `url(${PartnerBg.src})` }}
-      >
+        <section
+            className="relative bg-no-repeat py-14 !mt-0 lg:py-32 bg-cover w-full my-8 md:my-12  "
+            style={{ backgroundImage: `url(${PartnerBg.src})` }}
+        >
 
-        <div className="relative z-10 h-full flex mx-auto px-4   max-w-7xl">
-          <div className="max-w-2xl text-white text-center md:text-left pl-3   mt-5">
-            <h2 className=" w-full  text-3xl md:text-4xl leading-snug font-bold mb-8 md:mb-10 redhat">
-              {heading}
-            </h2>
-            <p
-              className="mb-4 text-sm  md:text-base   w-full"
-              dangerouslySetInnerHTML={{ __html: description }}
-            ></p>
-          </div>
-        </div>
-      </section>
+            <div className="relative z-10 h-full flex mx-auto px-4   max-w-7xl">
+                <div className="max-w-2xl text-white text-center md:text-left pl-3   mt-5">
+                    <h2 className=" w-full  text-3xl md:text-4xl leading-snug font-bold mb-8 md:mb-10 redhat">
+                        {heading}
+                    </h2>
+                    <p
+                        className="mb-4 text-sm  md:text-base   w-full"
+                        dangerouslySetInnerHTML={{ __html: description }}
+                    ></p>
+                </div>
+            </div>
+        </section>
     );
 }
