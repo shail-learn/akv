81c0ecf5facb156eff0ca6ad61f9c3c39bbdf9e5
zeesee123 <zubinchadha@gmail.com>
2025-07-25 15:24:38 +0530

    latest commit

--- src/app/components/home/impactHighlights.js
+++ src/app/components/home/impactHighlights.js
@@ -4,11 +4,11 @@ import icon1 from "../../../assets/images/home/leaf.svg";
 
 import Image from 'next/image';
 import Link from 'next/link';
-const ImpactHighlights = () => {
+const ImpactHighlights = ({props}) => {
     const heading ="What Sets Us Apart"
     const text = {
-        heading: 'We Believe in Making a Real Green Revolution',
-        title: 'At Atulye Krishi Vana, we are committed to building the world’s most impactful agri-company through sustainable agriculture and agroforestry. Our mission is to address climate change, global warming, and greenhouse gas emissions while fostering environmental resilience. <br> Our journey began in Madhya Pradesh, India, with a flagship project focused on sustainable forestry and organic plantations. By creating ecosystems that balance resource generation with environmental preservation, we aim to secure a prosperous and sustainable future for generations to come.'
+        heading: props?.data?.data?.sec4title??'We Believe in Making a Real Green Revolution',
+        title: props?.data?.data?.sec4_content??'At Atulye Krishi Vana, we are committed to building the world’s most impactful agri-company through sustainable agriculture and agroforestry. Our mission is to address climate change, global warming, and greenhouse gas emissions while fostering environmental resilience. <br> Our journey began in Madhya Pradesh, India, with a flagship project focused on sustainable forestry and organic plantations. By creating ecosystems that balance resource generation with environmental preservation, we aim to secure a prosperous and sustainable future for generations to come.'
     }
     const card = [
         {
